41201455b2fd4e50159ccae9d971edc518bd6112
Amit <amitrke@gmail.com>
2023-09-28 09:14:02 -0700

    Updated s3 script

--- shell/s3-upload.sh
+++ shell/s3-upload.sh
@@ -1,3 +1,10 @@
+#Check if an argument was passed
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
 # Filename using date and time
 filename=$(date '+%Y-%m-%d_%H-%M-%S').tar.gz
 
@@ -13,3 +20,7 @@ tar -zcvf $filename $filename
 # Upload file to S3, Year/Month/Day
 
 aws s3 cp $filename s3://$S3_BUCKET_NAME/$(date '+%Y')/$(date '+%m')/$(date '+%d')/$filename
+
+
+# Download file from S3
+aws s3 cp s3://$S3_BUCKET_NAME/$(date '+%Y')/$(date '+%m')/$(date '+%d')/$filename $filename
