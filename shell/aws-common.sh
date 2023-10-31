b4f5b7b10f0ba6c5ad176e97ba4d605c097b5391
Amit Kumar <amitrke@gmail.com>
2023-10-31 06:58:00 -0400

    Updated common script

--- shell/aws-common.sh
+++ shell/aws-common.sh
@@ -1,7 +1,26 @@
 # Use AWS CLI and jq to get the DNS of the load balancer whose name starts with "k8s-<cluster-name>-api-<random-string>"
 
-aws elbv2 describe-load-balancers \
+loadBalancerDNS=aws elbv2 describe-load-balancers \
   --query "LoadBalancers[?starts_with(LoadBalancerName, 'k8s-$CLUSTER_NAME-api-')].DNSName" \
   --output text \
   | sed 's/.*\(k8s-.*\)/\1/'
+```
+
+route53Record="nlb.$CLUSTER_NAME.$DOMAIN_NAME"
+
+#Update route53 DNS record
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID \
+  --change-batch "{
+    \"Comment\": \"Update NLB DNS record\",
+    \"Changes\": [{
+      \"Action\": \"UPSERT\",
+      \"ResourceRecordSet\": {
+        \"Name\": \"$route53Record\",
+        \"Type\": \"CNAME\",
+        \"TTL\": 300,
+        \"ResourceRecords\": [{\"Value\": \"$loadBalancerDNS\"}]
+      }
+    }]
+  }"
 ```
