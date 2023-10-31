2dca76a626820cc4b9ee16bea96136210590df4c
Amit Kumar <amitrke@gmail.com>
2023-10-31 07:46:31 -0400

    NLB zone id

--- shell/aws-common.sh
+++ shell/aws-common.sh
@@ -1,10 +1,12 @@
 # Use AWS CLI and jq to get the DNS of the load balancer whose name starts with "k8s-<cluster-name>-api-<random-string>"
 
-loadBalancerDNS=aws elbv2 describe-load-balancers \
+nlbDNS=$(aws elbv2 describe-load-balancers \
   --query "LoadBalancers[?starts_with(LoadBalancerName, 'k8s-$CLUSTER_NAME-api-')].DNSName" \
-  --output text \
-  | sed 's/.*\(k8s-.*\)/\1/'
-```
+  --output text)
+
+nlbZoneID=$(aws elbv2 describe-load-balancers \
+  --query "LoadBalancers[?DNSName=='$nlbDNS'].CanonicalHostedZoneId" \
+  --output text)
 
 #Route 53
 hostedZoneName="test.helloworld.com"
@@ -18,19 +20,23 @@ HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name \
 
 route53Record="nlb.$CLUSTER_NAME.$DOMAIN_NAME"
 
-#Update route53 DNS record
-aws route53 change-resource-record-sets \
-  --hosted-zone-id $HOSTED_ZONE_ID \
-  --change-batch "{
-    \"Comment\": \"Update NLB DNS record\",
-    \"Changes\": [{
-      \"Action\": \"UPSERT\",
-      \"ResourceRecordSet\": {
-        \"Name\": \"$route53Record\",
-        \"Type\": \"CNAME\",
-        \"TTL\": 300,
-        \"ResourceRecords\": [{\"Value\": \"$loadBalancerDNS\"}]
-      }
-    }]
-  }"
-```
+#Update route53 DNS record with NLB DNS and region
+
+aws 
+
+
+
+# aws route53 change-resource-record-sets \
+#   --hosted-zone-id $HOSTED_ZONE_ID \
+#   --change-batch "{
+#     \"Comment\": \"Update NLB DNS record\",
+#       \"Changes\": [{
+#         \"Action\": \"UPSERT\",
+#         \"ResourceRecordSet\": {
+#           \"Name\": \"$route53Record\",
+#           \"Type\": \"A\",
+#           \"TTL\": 300,
+#           \"ResourceRecords\": [{\"Value\": \"$nlbDNS\"}]
+#         }
+#       }]
+#     }",
