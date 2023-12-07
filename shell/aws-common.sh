1dc7a7dce5158127f3c945bd6701fe33bf6ee03f
Amit Kumar <amitrke@gmail.com>
2023-12-07 10:08:39 -0800

    Update route53 record with NLB DNS

--- shell/aws-common.sh
+++ shell/aws-common.sh
@@ -20,6 +20,33 @@ HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name \
 
 route53Record="nlb.$CLUSTER_NAME.$DOMAIN_NAME"
 
+#Get route53 record value
+route53RecordValue=$(aws route53 list-resource-record-sets \
+  --hosted-zone-id $HOSTED_ZONE_ID \
+  --query "ResourceRecordSets[?Name=='$route53Record.'].ResourceRecords[0].Value" \
+  --output text)
+
+#Check if the record53 record value is the same as the NLB DNS
+if [ "$route53RecordValue" != "$nlbDNS" ]; then
+  echo "Updating route53 record $route53Record to $nlbDNS"
+  aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOSTED_ZONE_ID \
+    --change-batch "{
+      \"Comment\": \"Update NLB DNS record\",
+        \"Changes\": [{
+          \"Action\": \"UPSERT\",
+          \"ResourceRecordSet\": {
+            \"Name\": \"$route53Record\",
+            \"Type\": \"A\",
+            \"TTL\": 300,
+            \"ResourceRecords\": [{\"Value\": \"$nlbDNS\"}]
+          }
+        }]
+      }"
+else
+  echo "Route53 record $route53Record is already up to date"
+fi
+
 #Update route53 DNS record with NLB DNS and region
 
 aws 
