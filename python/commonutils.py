39d608bf30edf3eceaa8842a2d076e4209426ed6
Amit Kumar <amitrke@gmail.com>
2023-12-19 00:47:25 +0000

    Update devcontainer.json and commonutils.py, add test_commonutils.py

--- python/commonutils.py
+++ python/commonutils.py
@@ -2,6 +2,13 @@ import datetime
 
 # Date Utils
 
-def get_previous_weekday(date: datetime.date) -> datetime.date:
-    """Get the previous weekday date from the given date"""
-    return date - datetime.timedelta(days=1 if date.weekday() == 0 else date.weekday())
+def get_previous_weekday(inputDate: datetime.date) -> datetime.date:
+    """Returns the date of the previous weekday (Mon-Fri)"""
+    # Monday = 0, Sunday = 6
+    if inputDate.weekday() == 0:
+        return inputDate - datetime.timedelta(days=3)
+    elif inputDate.weekday() == 6:
+        return inputDate - datetime.timedelta(days=2)
+    else:
+        return inputDate - datetime.timedelta(days=1)
+
