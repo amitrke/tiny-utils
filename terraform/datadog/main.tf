7267e9a0b445632f3d32a76850d424adb80dee01
Amit Kumar <amitrke@gmail.com>
2023-12-19 13:40:05 -0800

    Update error log monitor query

--- terraform/datadog/main.tf
+++ terraform/datadog/main.tf
@@ -1,17 +1,14 @@
 resource "datadog_monitor" "errorLogMonitor" {
     name = "Error Log Monitor"
     type = "log alert"
-    query = "logs('error', index='main')"
+    query = "count_over_time(logs('error').index('main').overAll().last('5m')) > 0"  # Adjust the time frame as needed
     message = "Error log found"
     escalation_message = "Error log found"
-    thresholds {
+    monitor_thresholds {
         critical = 1
     }
     notify_no_data = false
     renotify_interval = 60
     notify_audit = false
     timeout_h = 0
-    notify_thresholds {
-        critical = 1
-    }
 }
