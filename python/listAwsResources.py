21c328f2e01006d3dad29753b68c98d60ac42cb8
Amit Kumar <amitrke@gmail.com>
2023-12-21 16:56:27 -0800

    Add AWS resource listing functions and update config.json

--- python/listAwsResources.py
+++ python/listAwsResources.py
@@ -51,6 +51,40 @@ def getS3MultiRegionAccessPointList(region: str):
         })
     return accessPointList
 
+#Function to get list of all SSM Parameters
+def getSSMParameterList(region: str):
+    ssm = boto3.client('ssm', region_name=region)
+    parameterList = []
+    for parameter in ssm.describe_parameters()['Parameters']:
+        parameterList.append(parameter['Name'])
+    return parameterList
+
+#Function to get list of all Secrets Manager Secrets
+def getSecretsManagerSecretList(region: str):
+    secretsManager = boto3.client('secretsmanager', region_name=region)
+    secretList = []
+    for secret in secretsManager.list_secrets()['SecretList']:
+        secretList.append(secret['Name'])
+    return secretList
+
+#Function to get list of all Route53 DNS
+def getRoute53DNSList(region: str):
+    route53 = boto3.client('route53', region_name=region)
+    dnsList = []
+    for dns in route53.list_hosted_zones()['HostedZones']:
+        dnsList.append(dns['Name'])
+    return dnsList
+
+#Function to get a list of all ECR Repositories
+def getECRRepositoryList(region: str):
+    ecr = boto3.client('ecr', region_name=region)
+    ecrPrefix = config.get('AWS')['ecr']['repositoryPrefix']
+    repositoryList = []
+    for repository in ecr.describe_repositories()['repositories']:
+        if repository['repositoryName'].startswith(ecrPrefix):
+            repositoryList.append(repository['repositoryName'])
+    return repositoryList
+
 print("VPCs:")
 print("Primary Region: " + primaryRegion)
 print(getVpcList(primaryRegion))
@@ -62,3 +96,15 @@ print(getBucketList(primaryRegion))
 
 print("S3 Multi Region Access Points:")
 print(getS3MultiRegionAccessPointList(secondaryRegion))
+
+print("SSM Parameters:")
+print(getSSMParameterList(primaryRegion))
+
+print("Secrets Manager Secrets:")
+print(getSecretsManagerSecretList(primaryRegion))
+
+print("Route53 DNS:")
+print(getRoute53DNSList(primaryRegion))
+
+print("ECR Repositories:")
+print(getECRRepositoryList(primaryRegion))
