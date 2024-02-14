45d2cef52dcba1362415bf438158f7aae35b54f8
Amit Kumar <amitrke@gmail.com>
2024-02-14 15:55:50 -0800

    Add k8-cron.sh script for enabling/disabling cronjobs in k8s

---
+++ shell/k8-cron.sh
@@ -0,0 +1,21 @@
+#Usage: k8-cron.sh <namespace> <enable/disable>
+#Description: Enable/Disable cronjob in k8s
+
+#!/bin/bash
+
+#Check input arguments
+if [ -z "$1" ] || [ -z "$2" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+CLUSTER_NAME="k8s-cluster"
+REGION="us-west-2"
+
+#Set the context
+aws eks --region $REGION update-kubeconfig --name $CLUSTER_NAME
+
+#Enable/Disable all cronjobs in the namespace
+kubectl get cronjobs -n $1 | awk '{print $1}' | grep -v NAME | xargs -I {} kubectl patch cronjob {} -n $1 -p '{"spec":{"suspend":'$2'}}'
+```
